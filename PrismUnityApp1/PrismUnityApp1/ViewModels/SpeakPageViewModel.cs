20e40918dbcad826607db52c6bb04fa6dcbac400
Keyvin Duque <thkeyduq@gmail.com>
2017-08-15 23:16:37 -0400

    project init

---
+++ PrismUnityApp1/PrismUnityApp1/ViewModels/SpeakPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrismUnityApp1.ViewModels
+{
+    public class SpeakPageViewModel : BindableBase
+    {
+        private string _txtToSay = "Hola Camaradas";
+        public string TextToSay
+        {
+            get { return _txtToSay; }
+            set { SetProperty(ref _txtToSay, value); }
+        }
+        public DelegateCommand SpeakCommand { get; set; }
+        public SpeakPageViewModel()
+        {
+            SpeakCommand = new DelegateCommand(Speak);
+
+        }
+        private void Speak()
+        {
+            //TODO: speak
+        }
+    }
+}
